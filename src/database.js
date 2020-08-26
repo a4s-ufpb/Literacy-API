0aca3e0b9e5bc5b34f720eece6c70ee4369ab515
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-08-26 16:09:32 -0300

    Adcionado senha do banco

--- src/database.js
+++ src/database.js
@@ -6,8 +6,8 @@ import Sequelize from 'sequelize'
 //     storage: './data.sqlite'
 // })
 
-export let sequelize = new Sequelize('literacy', process.env.DB_USER || 'postgres', process.env.DB_PASSWORD || 'postgres', {
-    host: 'localhost',
+export let sequelize = new Sequelize('literacy', process.env.DB_USER || 'postgres', process.env.DB_PASSWORD || '8HdQy7vkai6B34', {
+    host: 'db',
     port: '5432',
     dialect: 'postgres',
     operatorsAliases: false
